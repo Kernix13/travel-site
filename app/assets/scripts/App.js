2fb8be7ab69448a8367b1ace10d766a8621434ec
“Jim” <jimkernicky@gmail.com>
2021-11-03 17:49:48 -0400

    Completed Sticky Headers and scroll features

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -14,13 +14,11 @@ class RevealOnScroll {
   events() {
     window.addEventListener("scroll", this.scrollThrottle)
     window.addEventListener("resize", debounce(() => {
-      console.log("Resize just ran")
       this.browserHeight = window.innerHeight
     }, 333))
   }
 
   calcCaller() {
-    console.log("Scroll function ran")
     this.itemsToReveal.forEach(el => {
       if (el.isRevealed == false) {
         this.calculateIfScrolledTo(el)
@@ -30,7 +28,6 @@ class RevealOnScroll {
 
   calculateIfScrolledTo(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop) {
-      console.log("Element was calculated")
       let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
       if (scrollPercent < this.thresholdPercent) {
         el.classList.add("reveal-item--is-visible")
