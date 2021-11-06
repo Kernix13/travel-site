c7ea21b049c2c4e9fcdb873d14a587278cbf05e0
“Jim” <jimkernicky@gmail.com>
2021-11-05 20:15:23 -0400

    Completed lazy loading of images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
