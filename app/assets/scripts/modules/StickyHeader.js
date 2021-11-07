5cf2f657000c1e49ff658667928ad3fcc36df736
“Jim” <jimkernicky@gmail.com>
2021-11-07 18:03:32 -0500

    Minor change

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -38,7 +38,7 @@ class StickyHeader {
 
   calcSection(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
-      let scrollPercent = el.getBoundingClientRect().y / this.browserHeight * 100
+      let scrollPercent = el.getBoundingClientRect().top / this.browserHeight * 100
       if (scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection == 'down' || scrollPercent < 33 && this.scrollDirection == 'up') {
         let matchingLink = el.getAttribute("data-matching-link")
         document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"))
