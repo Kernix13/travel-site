ab5caee20f124b13285fd3c8b9a760f145c7d8ce
“Jim” <jimkernicky@gmail.com>
2021-11-08 18:21:08 -0500

    Added client area feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,7 +3,9 @@ import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+import ClientArea from './modules/ClientArea'
 
+new ClientArea()
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
---
+++ app/assets/scripts/modules/ClientArea.js
@@ -0,0 +1,46 @@
+import Axios from 'axios'
+
+class ClientArea {
+  constructor() {
+    this.injectHTML()
+    this.form = document.querySelector(".client-area__form")
+    this.field = document.querySelector(".client-area__input")
+    this.contentArea = document.querySelector(".client-area__content-area")
+    this.events()
+  }
+
+  events() {
+    this.form.addEventListener("submit", e => {
+      e.preventDefault()
+      this.sendRequest()
+    })
+  }
+
+  sendRequest() {
+    Axios.post('https://gallant-hamilton-e0c06d.netlify.app/.netlify/functions/secret-area', { password: this.field.value }).then(response => {
+      this.form.remove()
+      this.contentArea.innerHTML = response.data
+    }).catch(() => {
+      this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`
+      this.field.value = ''
+      this.field.focus()
+    })
+  }
+
+  injectHTML() {
+    document.body.insertAdjacentHTML('beforeend', `
+    <div class="client-area">
+      <div class="wrapper wrapper--medium">
+        <h2 class="section-title section-title--blue">Secret Client Area</h2>
+        <form class="client-area__form" action="">
+          <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
+          <button class="btn btn--orange">Submit</button>
+        </form>
+        <div class="client-area__content-area"></div>
+      </div>
+    </div>
+    `)
+  }
+}
+
+export default ClientArea
